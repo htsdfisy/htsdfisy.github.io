17a417eacb38431a1cb74f08db7c45da3d4cbf62
htsdfisy <132798839+htsdfisy@users.noreply.github.com>
2024-05-27 21:45:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 30) {
@@ -5,4 +6,24 @@ window.addEventListener("scroll", () => {
     } else {
         header.classList.remove("scrolled");
     }
+});
+
+const indbody = document.getElementById('ind-body');
+const achbody = document.getElementById('ach-body');
+const miku = document.querySelector(".miku");
+const hollow = document.querySelector(".hollow");
+const ach = document.getElementById('ach-title')
+miku.addEventListener("click",()=>{
+	miku.style.display = "none";
+	hollow.style.display = "block";
+	indbody.style.backgroundImage = "url(images/pxfuel.jpg)";
+	achbody.style.backgroundImage = "url(images/mikubglong.jpg)";
+	ach.src = "images/achievementb.png";
+});
+hollow.addEventListener("click",()=>{
+	miku.style.display = "block";
+	hollow.style.display = "none";
+	achbody.style.backgroundImage = "url(images/1360353.png)";
+	indbody.style.backgroundImage = "url(images/1360353.png)";
+	ach.src = "images/achievement.png";
 });
